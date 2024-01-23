918b4c095ed7bcfa536b43b9e1f214f3d8029965
Evgenii Strelets <StreletsEV@anodialog.ru>
2024-01-23 13:10:18 +0700

    cron scripts

---
+++ cron/cloud.sh
@@ -0,0 +1,3 @@
+#!/bin/sh
+
+docker compose -f /home/estr/HomeServerUtils/docker-compose.yaml run rclone sync -v yandex:Фотокамера/Ника /mount/media/photos
---
+++ cron/photo.sh
@@ -0,0 +1,3 @@
+#!/bin/sh
+
+docker compose -f /home/estr/HomeServerUtils/docker-compose.yaml exec photoprism photoprism index --cleanup
