dbaed67be0c1d5d35e6f6c85bd97b92c527be9f9
Evgenii Strelets <StreletsEV@anodialog.ru>
2024-02-02 19:23:04 +0700

    add converter

--- cron/cloud.sh
+++ cron/cloud.sh
@@ -1,3 +1,4 @@
 #!/bin/sh
 
+docker compose -f /home/estr/HomeServerUtils/docker-compose.yaml run converter
 docker compose -f /home/estr/HomeServerUtils/docker-compose.yaml run rclone sync -v yandex:Фотокамера/Ника /mount/photos
